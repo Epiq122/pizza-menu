cf9815e966a470e8ba4ec2fe78c2681c7d6aac05
Robert Gleason <l2ob122@gmail.com>
2023-06-06 18:31:09 -0700

    created footer,menu,header component

--- src/App.jsx
+++ src/App.jsx
@@ -48,11 +48,33 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      {new Date().toLocaleTimeString()}
+      <p>We&apos;re currently open</p>
+    </footer>
   );
 }
 
