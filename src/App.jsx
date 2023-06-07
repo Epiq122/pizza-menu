7e52442eca1a664f9076837f0201a501d3efff08
Robert Gleason <l2ob122@gmail.com>
2023-06-06 20:48:59 -0700

    made it so the pizzas are coming from the map now

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 const pizzaData = [
   {
-    name: 'Focaccia',
-    ingredients: 'Bread with italian olive oil and rosemary',
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: 'pizzas/focaccia.jpg',
+    photoName: "pizzas/focaccia.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Margherita',
-    ingredients: 'Tomato and mozarella',
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: 'pizzas/margherita.jpg',
+    photoName: "pizzas/margherita.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Spinaci',
-    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: 'pizzas/spinaci.jpg',
+    photoName: "pizzas/spinaci.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Funghi',
-    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: 'pizzas/funghi.jpg',
+    photoName: "pizzas/funghi.jpg",
     soldOut: false,
   },
   {
-    name: 'Pizza Salamino',
-    ingredients: 'Tomato, mozarella, and pepperoni',
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: 'pizzas/salamino.jpg',
+    photoName: "pizzas/salamino.jpg",
     soldOut: true,
   },
   {
-    name: 'Pizza Prosciutto',
-    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: 'pizzas/prosciutto.jpg',
+    photoName: "pizzas/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -64,28 +64,16 @@ function Header() {
     </header>
   );
 }
+
 function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
-        name="Pizza Margherita"
-        ingredients="Tomato and mozarella"
-        photoName="pizzas/margherita.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Focaccia"
-        ingredients="Bread with italian olive oil and rosemary"
-        photoName="pizzas/focaccia.jpg"
-        price={6}
-      />
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={12}
-      />
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
     </main>
   );
 }
@@ -107,16 +95,16 @@ function Footer() {
   );
 }
 
-function Pizza({ photoName, name, ingredients, price }) {
+function Pizza({ pizzaObj }) {
   return (
-    <div className="pizza">
-      <img src={photoName} alt={name} />
+    <li className="pizza">
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{name}</h3>
-        <p>{ingredients}</p>
-        <span>{price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
